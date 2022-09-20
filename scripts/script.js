96be897e421d10b7e57988e036707d06d972f223
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-09-20 15:30:10 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 
       var imageNodes = document.getElementsByTagName('img');
-      console.log(imageNodes.length);
+      console.log(imageNodes[1].src);
       for (var i=0; i<imageNodes.length; i++){          
             imageNodes[i].addEventListener('click', function() {
                   document.querySelector('.main').src =  imageNodes[i].src;
