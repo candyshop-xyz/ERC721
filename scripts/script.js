f2e0796176c057631dff0cb79d0bc764c4df85d0
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-09-20 16:15:51 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,40 +6,40 @@ document.querySelector('body').classList.toggle('open');
 
 imageNodes[1].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[1].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
 
 imageNodes[2].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[2].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
 
 
 imageNodes[3].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[3].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
 
 
 imageNodes[4].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[4].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
 
 
 imageNodes[5].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[5].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
 
 
 imageNodes[6].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[6].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
 
 
 imageNodes[7].addEventListener('click', function(){
 document.querySelector('.main').src = imageNodes[7].src;
-document.querySelector('body').classList.toggle('open'); 
+document.querySelector('body').classList.remove('open'); 
 });
