bcc4580a9192edbb2b0d42496d4d3fb3ea5a42ed
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-09-20 15:56:10 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 var imageNodes = document.getElementsByTagName('img');
+function selectImage(index) {
+console.log(imageNodes[index].src)
+document.querySelector('.main').src = imageNodes[index].src;
+document.querySelector('body').classList.remove('open');
+}
 for (var i=1; i<imageNodes.length; i++){          
             console.log(imageNodes[i].src)
-            imageNodes[i].addEventListener('click', function() {
-                  console.log(imageNodes[i].src)
-                  document.querySelector('.main').src = imageNodes[i].src;
-                  document.querySelector('body').classList.remove('open');
-            });
+            imageNodes[i].addEventListener('click', selectImage(i);
       }
       function toggle() {
         document.querySelector('body').classList.toggle('open');
