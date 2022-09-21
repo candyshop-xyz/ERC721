4a5ce59bc06c8091aad3e058e718948922acbd2d
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-09-20 22:39:34 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,56 @@
 var imageNodes = document.getElementsByTagName('img');
 
+imageNodes[1].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[1].src;
+  document.querySelector('body').classList.remove('open');
+}
+
+imageNodes[2].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[2].src;
+  document.querySelector('body').classList.remove('open');
+}
+
+imageNodes[3].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[3].src;
+  document.querySelector('body').classList.remove('open');
+}
+
+imageNodes[4].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[4].src;
+  document.querySelector('body').classList.remove('open');
+}
+
+imageNodes[5].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[5].src;
+  document.querySelector('body').classList.remove('open');
+}
+
+imageNodes[6].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[6].src;
+  document.querySelector('body').classList.remove('open');
+}
+
+imageNodes[7].onclick = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.main').src = imageNodes[7].src;
+  document.querySelector('body').classList.remove('open');
+}
+
 function toggle() {
 document.querySelector('body').classList.toggle('open');
 }
 
-imageNodes[1].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[1].src;
-document.querySelector('body').classList.remove('open'); 
-});
 
-imageNodes[2].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[2].src;
-document.querySelector('body').classList.remove('open'); 
-});
-
-
-imageNodes[3].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[3].src;
-document.querySelector('body').classList.remove('open'); 
-});
-
-
-imageNodes[4].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[4].src;
-document.querySelector('body').classList.remove('open'); 
-});
-
-
-imageNodes[5].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[5].src;
-document.querySelector('body').classList.remove('open'); 
-});
-
-
-imageNodes[6].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[6].src;
-document.querySelector('body').classList.remove('open'); 
-});
-
-
-imageNodes[7].addEventListener('click', function(){
-document.querySelector('.main').src = imageNodes[7].src;
-document.querySelector('body').classList.remove('open'); 
-});
